1ce439f1f0b9d6740dfa47a60681965e97cd1c46
刘 一澍 <yishuliu@meilishuo.com>
2015-10-16 11:59:11 +0800

    add files

---
+++ config.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	Url        string        `json:"url"`
+	Zookeepers []string      `json:"zookeepers"`
+	ZkPath     string        `json:"zk_path"`
+	ZkCluster  string        `json:"zk_cluster"`
+	Interval   int           `json:"interval"`
+	Distance   int           `json:"distance_threshold"`
+	Passby     []Clusterlist `json:"pass_by"`
+}
+
+type Clusterlist struct {
+	Cluster       string `json:"cluster"`
+	ConsumerGroup string `json:"consumer_group"`
+	Topic         string `json:"topic"`
+}
+
+func loadConfig(configFile string) (*Config, error) {
+	var c *Config
+	path := configFile
+	fi, err := os.Open(path)
+	defer fi.Close()
+	if nil != err {
+		return nil, err
+	}
+	defer fi.Close()
+
+	fd, err := ioutil.ReadAll(fi)
+
+	err = json.Unmarshal([]byte(fd), &c)
+	if nil != err {
+		return nil, err
+	}
+	return c, nil
+}
